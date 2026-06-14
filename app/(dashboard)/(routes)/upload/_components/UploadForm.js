e3ac43871ba48dc085ec93d325def74d8e64a4a3
Deepanshu Khatri <deepanshukhatri20061972@gmail.com>
2026-06-15 01:06:22 +0530

    Spacing Fix

--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -70,7 +70,7 @@ function UploadForm({uploadBtnClick}) {
                         <svg className="w-12 h-12 sm:w-14 sm:h-14 mb-3 sm:mb-4 text-primary" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h3a3 3 0 0 0 0-6h-.025a5.56 5.56 0 0 0 .025-.5A5.5 5.5 0 0 0 7.207 9.021C7.137 9.017 7.071 9 7 9a4 4 0 1 0 0 8h2.167M12 19v-9m0 0-2 2m2-2 2 2" /></svg>
                         <p className="mb-2 text-sm px-2">
                             <strong className='text-primary font-extrabold text-base sm:text-lg block sm:inline-block'>Click to upload </strong>
-                            <span className='text-base sm:text-lg font-medium text-gray-550 block sm:inline-block'>or drag and drop</span>
+                            <span className='text-base sm:text-lg font-medium text-gray-550 block sm:inline-block'> or drag and drop</span>
                         </p>
                         <p className="text-xs sm:text-[13px] text-gray-400">SVG, PNG, JPG or GIF (Max 2MB per file)</p>
                     </div>
